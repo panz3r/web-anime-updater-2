cb04c55f38af5401f8dd0085a1915dd1461b8855
Mattia Panzeri <mattia.panzeri93@gmail.com>
2018-01-02 14:07:04 +0100

    NodeJS BE - Improve NodeJS BE entrypoint script

--- backend/nodejs/src/index.js
+++ backend/nodejs/src/index.js
@@ -1,28 +1,56 @@
+import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
-import express from 'express'
+import Express from 'express'
 import jwt from 'express-jwt'
+import { get } from 'lodash'
+import Logger from 'pretty-logger'
 
-import { createTokenForUser } from './auth'
+import { AuthManager } from './auth'
+import { ExpressErrorManager } from './common'
 
+// Load .env
 dotenv.config()
 
+// Global constants
+const serverPort = 3000
 const apiEndpoint = '/api/v1'
-const app = express()
 
-app.use(jwt({ secret: process.env.JWT_SECRET })
-  .unless({ path: [`${apiEndpoint}/login`] }))
+// Logger
+const log = new Logger()
+
+// Configure Express (init, middlewares, etc.)
+const app = Express()
+
+app.use(jwt({ secret: process.env.JWT_SECRET }) // Support JWT authentication
+  .unless({
+    path: [
+      `${apiEndpoint}/register`,
+      `${apiEndpoint}/login`
+    ]
+  }))
+
+app.use(bodyParser.json()); // Support parsing of application/json type post data
+
+// Define endpoints
+app.post(`${apiEndpoint}/register`, (req, res) => {
+  AuthManager.newUser(get(req, 'body', {}))
+    .then(() => {
+      res.sendStatus(201)
+    })
+    .catch(err => {
+      log.error(`${err.code}: ${err.status}`)
+      ExpressErrorManager.sendError(res, err)
+    })
+})
 
 app.post(`${apiEndpoint}/login`, (req, res) => {
-  // TODO Authenticate user and generate his token
-  createTokenForUser("abc123")
+  AuthManager.login(get(req, 'body', {}))
     .then(token => {
       res.json({ token })
     })
     .catch(err => {
-      console.log('Error', err)
-      // HTTP status 500: Internal Server Error
-      res.status(500)
-        .send('Internal Server Error')
+      log.error(`${err.code}: ${err.status}`)
+      ExpressErrorManager.sendError(res, err)
     })
 })
 
@@ -30,4 +58,14 @@ app.get(`${apiEndpoint}/notes`, (req, res) => {
   res.json({ notes: "This is your notebook. Edit this to start saving your notes!" })
 })
 
-app.listen(3000)
+// Start Express server
+log.info(`Starting server on port ${serverPort}...`)
+const server = app.listen(serverPort, 'localhost', function (err) {
+  const { address, port } = server.address()
+  if (err) {
+    log.error(`Unable to listen on port ${port}`, error)
+    process.exit(1)
+  }
+
+  log.info(`Server listening at http://${address}:${port}`)
+});
