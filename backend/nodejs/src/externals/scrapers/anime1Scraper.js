5f7314d78adde1af665073d7712d87799356e315
Mattia Panzeri <mattia.panzeri93@gmail.com>
2018-01-13 19:57:15 +0100

    NodeJS BE - Improve `Anime1Scraper` class

--- backend/nodejs/src/externals/scrapers/anime1Scraper.js
+++ backend/nodejs/src/externals/scrapers/anime1Scraper.js
@@ -1,10 +1,10 @@
 import Cheerio from 'cheerio'
-import Cloudscraper from 'cloudscraper'
 import {
   capitalize,
   each,
   get,
   last,
+  map,
   parseInt,
   replace,
   set,
@@ -14,6 +14,8 @@ import {
 } from 'lodash'
 import { Duration } from 'luxon'
 import Logger from 'pretty-logger'
+
+import { Cloudscraper } from './utils'
 
 const log = Logger({
   prefix: 'Anime1Scraper'
@@ -32,78 +34,107 @@ export class Anime1Scraper {
         'minutes'
       )} minutes`
     )
-    setInterval(this._scrapeSite, scheduleTimer)
+    setInterval(this._updateTask, scheduleTimer)
   }
 
-  async _scrapeSite() {
-    log.info(`Scraping started...`)
+  async _updateTask() {
+    log.info(`Update task started for Anime1 scraper...`)
 
     const series = await this.sm.getAllSeries()
-    each(series, serie => {
-      const pageUri = get(serie, 'url')
+    each(series, async serie => {
+      const updatesData = await this._scrapeSite(get(serie, 'url'))
+      if (updatesData) {
+        // Update serie details on DB
+        const serieTitle = get(updatesData, 'serie.title')
+        await this.sm.updateSerie(serie.id, {
+          ...serie,
+          title: serieTitle,
+          posterUrl: get(updatesData, 'serie.posterUrl')
+        })
+        log.info(`Updated '${serieTitle || series.title}' serie details`)
 
-      if (pageUri.startsWith('http://www.anime1.com')) {
-        log.info(`Scraping page at '${pageUri}'...`)
-
-        Cloudscraper.get(pageUri, async (err, response, body) => {
-          if (err) {
-            log.error(`Failed to fetch '${pageUri}'`, err)
-          } else {
-            const page = Cheerio.load(body)
-
-            // Serie title
-            const seriesTitleElems = page(
-              '.anime > .detail-left > h1.blue-main-title'
-            )
-            const seriesTitle =
-              size(seriesTitleElems) > 0
-                ? trim(seriesTitleElems.eq(0).text())
-                : undefined
-
-            // Series poster
-            let posterUrl = undefined
-            try {
-              const seriesPosterElems = page('.anime > .detail-cover')
-              posterUrl =
-                size(seriesPosterElems) > 0
-                  ? seriesPosterElems
-                      .eq(0)
-                      .find('img')
-                      .eq(0)
-                      .attr('src')
-                  : undefined
-            } catch (err) {
-              log.error(`Couldn't find Serie poster`, err)
-            }
-
-            // Update serie details
-            await this.sm.updateSerie(serie.id, {
-              ...serie,
-              title: seriesTitle,
-              posterUrl
-            })
-            log.info(`Updated '${seriesTitle}' serie details`)
-
-            // Episodes
-            const episodesLink = page('ul.anime-list > li > a')
-            log.debug(`Found ${size(episodesLink)} links`)
-            const episodes = {}
-            episodesLink.each((i, elem) => {
-              const href = get(elem, 'attribs.href', '')
-              const title = last(split(href, '/'))
-              set(episodes, title, {
-                number: parseInt(replace(title, 'episode-', '')),
-                title: capitalize(replace(title, '-', ' ')),
-                url: href
-              })
-            })
-
-            log.info(
-              `Found ${size(episodes)} episodes for '${seriesTitle}' serie.`
-            )
-          }
+        // Update episodes details on DB
+        each(updatesData.episodes, e => {
+          console.log({ e })
         })
       }
     })
+  }
+
+  async _scrapeSite(pageUri) {
+    const scrapedData = {
+      serie: {
+        title: undefined,
+        posterUrl: undefined
+      },
+      episodes: []
+    }
+
+    if (!pageUri.startsWith('http://www.anime1.com')) {
+      log.debug(`Anime1Scraper NOT fit for '${pageUri}' url. Won't scrape it.`)
+      return
+    }
+
+    log.info(`Scraping page at '${pageUri}'...`)
+
+    // Scrape Anime1 page
+    const { err, response, body } = await Cloudscraper.get(pageUri)
+    if (err) {
+      log.error(`Failed to fetch '${pageUri}'`, err)
+      return
+    }
+
+    const page = Cheerio.load(body)
+
+    // Serie title
+    const seriesTitleElems = page('.anime > .detail-left > h1.blue-main-title')
+    const seriesTitle =
+      size(seriesTitleElems) > 0 ? trim(seriesTitleElems.eq(0).text()) : undefined
+    set(scrapedData, 'serie.title', seriesTitle)
+
+    // Series poster
+    let posterUrl = undefined
+    try {
+      const seriesPosterElems = page('.anime > .detail-cover')
+      if (size(seriesPosterElems) > 0) {
+        posterUrl = seriesPosterElems
+          .eq(0)
+          .find('img')
+          .eq(0)
+          .attr('src')
+
+        set(scrapedData, 'serie.posterUrl', posterUrl)
+      }
+    } catch (err) {
+      log.error(`Couldn't find Serie poster`, err)
+    }
+
+    // Episodes
+    const episodesLink = page('ul.anime-list > li > a')
+    log.debug(`Found ${size(episodesLink)} links`)
+    const episodes = {}
+    episodesLink.each((i, elem) => {
+      const href = get(elem, 'attribs.href', '')
+      const title = last(split(href, '/'))
+      set(episodes, title, {
+        number: parseInt(replace(title, 'episode-', '')),
+        title: capitalize(replace(title, '-', ' ')),
+        url: href
+      })
+    })
+
+    log.info(`Found ${size(episodes)} episodes for '${seriesTitle}' serie.`)
+
+    set(
+      scrapedData,
+      'episodes',
+      map(episodes, episode => ({
+        number: episode.number,
+        title: episode.title,
+        url: episode.url
+      }))
+    )
+
+    return scrapedData
   }
 }
