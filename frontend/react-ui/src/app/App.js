307ce03ae3443ec643b8f8cb9ca76f26e0f46249
Mattia Panzeri <mattia.panzeri93@gmail.com>
2018-01-29 16:03:03 +0100

    React FE - Add `redux-persist`

--- frontend/react-ui/src/app/App.js
+++ frontend/react-ui/src/app/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/lib/integration/react'
+import { CircularProgress } from 'material-ui/Progress'
 
-import { LoginPage, NotFoundPage } from './pages'
-import store from './state'
+import { PrivateRoute } from './commons'
+import { LoginPage, AnimePage, NotFoundPage } from './pages'
+import { store, persistor } from './state'
 
 const App = () => (
   <Provider store={store}>
-    <Router>
-      <Switch>
-        <Route exact path="/login" component={LoginPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <PersistGate loading={<CircularProgress />} persistor={persistor}>
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={LoginPage} />
+          <PrivateRoute path="/" component={AnimePage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
+    </PersistGate>
   </Provider>
 )
 
--- frontend/react-ui/src/app/state/index.js
+++ frontend/react-ui/src/app/state/index.js
@@ -1,4 +1,6 @@
 import { combineReducers, compose, createStore } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import user from './user'
 
@@ -11,4 +13,13 @@ const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose
 
-export default createStore(reducers, {}, composeEnhancers())
+// Redux Persist
+const persistConfig = {
+  key: 'wau2-react-ui',
+  storage: storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+export const store = createStore(persistedReducer, {}, composeEnhancers())
+export const persistor = persistStore(store)
